92034d0f2a082a55be083777526fca6fc242a167
Nicu Ioana <ioana.nicu@cegeka.com>
2021-06-24 19:18:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlaywrightDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
